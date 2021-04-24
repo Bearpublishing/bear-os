b543e027a77dfbfc49a3209c1c21759aeb079d8d
Bearpublishing <81031196+Bearpublishing@users.noreply.github.com>
2021-04-24 14:02:29 +0100

    Added home screen movment

--- main.ts
+++ main.ts
@@ -1,5 +1,25 @@
+input.onButtonPressed(Button.A, function () {
+    if (mode == "Start") {
+        Cruser.change(LedSpriteProperty.X, 1)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (mode == "Start") {
+        if (Cruser == 0) {
+        	
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (mode == "Start") {
+        Cruser.change(LedSpriteProperty.X, -1)
+    }
+})
+let Cruser: game.LedSprite = null
+let mode = ""
 basic.showString("Welcome to Bear OS!")
-let Cruser = game.createSprite(0, 1)
+mode = "Start"
+Cruser = game.createSprite(0, 1)
 let App_10 = game.createSprite(0, 0)
 let App_9 = game.createSprite(1, 0)
 let App_8 = game.createSprite(2, 0)
