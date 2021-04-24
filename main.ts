ddd5bd1a1f9f07cb728b9f5b763b48217c6e32fd
Bearpublishing <81031196+Bearpublishing@users.noreply.github.com>
2021-04-24 13:47:02 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
 basic.showString("Welcome to Bear OS!")
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    # # # # #
-    . . . . .
-    # # # # #
-    `)
+let Cruser = game.createSprite(0, 1)
+let App_10 = game.createSprite(0, 0)
+let App_9 = game.createSprite(1, 0)
+let App_8 = game.createSprite(2, 0)
+let App_7 = game.createSprite(3, 0)
+let App_6 = game.createSprite(4, 0)
+let App_5 = game.createSprite(5, 0)
+let App_4 = game.createSprite(2, 2)
+let App_3 = game.createSprite(2, 2)
+let App_2 = game.createSprite(2, 2)
+let App_1 = game.createSprite(2, 2)
