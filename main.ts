c19fb936ee0803dc69a8680a5e01ef1055daacf5
Oliver Edge <BearPublishing1@gmail.com>
2021-04-25 16:22:08 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -48,16 +48,6 @@ Crusor_pos = Cruser.get(LedSpriteProperty.X)
 crusor_pos_y = Cruser.get(LedSpriteProperty.Y)
 basic.forever(function () {
     if (mode == "App 1") {
-        Cruser = game.createSprite(0, 1)
-        App_10 = game.createSprite(0, 0)
-        App_9 = game.createSprite(1, 0)
-        App_8 = game.createSprite(2, 0)
-        App_7 = game.createSprite(3, 0)
-        App_6 = game.createSprite(4, 0)
-        App_5 = game.createSprite(0, 2)
-        App_4 = game.createSprite(1, 2)
-        App_3 = game.createSprite(2, 2)
-        App_2 = game.createSprite(3, 2)
-        App_1 = game.createSprite(4, 2)
+    	
     }
 })
